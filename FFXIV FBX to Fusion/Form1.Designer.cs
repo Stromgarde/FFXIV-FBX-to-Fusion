9ef962657bc0c0e1f33c0412a68fb2db7dcbf99f
Stromgarde <epollino@gmail.com>
2024-04-15 09:33:55 -0500

    Add project files.

---
+++ FFXIV FBX to Fusion/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace FFXIV_FBX_to_Fusion
+{
+    partial class FFXIV_FBX_to_Fusion
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            blender_location_button = new Button();
+            convert_button = new Button();
+            specular_button = new RadioButton();
+            panel1 = new Panel();
+            emissive_button = new RadioButton();
+            opacity_button = new RadioButton();
+            diffuse_button = new RadioButton();
+            select_fbx_button = new Button();
+            blender_path_label = new Label();
+            fbx_path_label = new Label();
+            debug_label = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // blender_location_button
+            // 
+            blender_location_button.Location = new Point(125, 12);
+            blender_location_button.Name = "blender_location_button";
+            blender_location_button.Size = new Size(249, 23);
+            blender_location_button.TabIndex = 0;
+            blender_location_button.Text = "Select Blender Executable";
+            blender_location_button.UseVisualStyleBackColor = true;
+            blender_location_button.Click += blender_location_button_Click;
+            // 
+            // convert_button
+            // 
+            convert_button.Location = new Point(212, 126);
+            convert_button.Name = "convert_button";
+            convert_button.Size = new Size(75, 23);
+            convert_button.TabIndex = 1;
+            convert_button.Text = "Convert";
+            convert_button.UseVisualStyleBackColor = true;
+            convert_button.Click += convert_button_Click;
+            // 
+            // specular_button
+            // 
+            specular_button.AutoSize = true;
+            specular_button.Location = new Point(21, 15);
+            specular_button.Name = "specular_button";
+            specular_button.Size = new Size(70, 19);
+            specular_button.TabIndex = 2;
+            specular_button.TabStop = true;
+            specular_button.Text = "Specular";
+            specular_button.UseVisualStyleBackColor = true;
+            specular_button.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(emissive_button);
+            panel1.Controls.Add(opacity_button);
+            panel1.Controls.Add(diffuse_button);
+            panel1.Controls.Add(specular_button);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(107, 137);
+            panel1.TabIndex = 3;
+            // 
+            // emissive_button
+            // 
+            emissive_button.AutoSize = true;
+            emissive_button.Location = new Point(21, 90);
+            emissive_button.Name = "emissive_button";
+            emissive_button.Size = new Size(70, 19);
+            emissive_button.TabIndex = 5;
+            emissive_button.TabStop = true;
+            emissive_button.Text = "Emissive";
+            emissive_button.UseVisualStyleBackColor = true;
+            // 
+            // opacity_button
+            // 
+            opacity_button.AutoSize = true;
+            opacity_button.Location = new Point(21, 65);
+            opacity_button.Name = "opacity_button";
+            opacity_button.Size = new Size(66, 19);
+            opacity_button.TabIndex = 4;
+            opacity_button.TabStop = true;
+            opacity_button.Text = "Opacity";
+            opacity_button.UseVisualStyleBackColor = true;
+            // 
+            // diffuse_button
+            // 
+            diffuse_button.AutoSize = true;
+            diffuse_button.Location = new Point(21, 40);
+            diffuse_button.Name = "diffuse_button";
+            diffuse_button.Size = new Size(62, 19);
+            diffuse_button.TabIndex = 3;
+            diffuse_button.TabStop = true;
+            diffuse_button.Text = "Diffuse";
+            diffuse_button.UseVisualStyleBackColor = true;
+            // 
+            // select_fbx_button
+            // 
+            select_fbx_button.Location = new Point(125, 73);
+            select_fbx_button.Name = "select_fbx_button";
+            select_fbx_button.Size = new Size(249, 23);
+            select_fbx_button.TabIndex = 4;
+            select_fbx_button.Text = "Select FBX file";
+            select_fbx_button.UseVisualStyleBackColor = true;
+            select_fbx_button.Click += fbx_location_button_Click;
+            // 
+            // blender_path_label
+            // 
+            blender_path_label.AutoSize = true;
+            blender_path_label.Location = new Point(125, 39);
+            blender_path_label.Name = "blender_path_label";
+            blender_path_label.Size = new Size(134, 15);
+            blender_path_label.TabIndex = 5;
+            blender_path_label.Text = "Blender Executable Path";
+            blender_path_label.Click += blender_path_label_Click;
+            // 
+            // fbx_path_label
+            // 
+            fbx_path_label.AutoSize = true;
+            fbx_path_label.Location = new Point(125, 99);
+            fbx_path_label.Name = "fbx_path_label";
+            fbx_path_label.Size = new Size(75, 15);
+            fbx_path_label.TabIndex = 6;
+            fbx_path_label.Text = "FBX File Path";
+            // 
+            // debug_label
+            // 
+            debug_label.AutoSize = true;
+            debug_label.Location = new Point(35, 197);
+            debug_label.MaximumSize = new Size(490, 0);
+            debug_label.Name = "debug_label";
+            debug_label.Size = new Size(71, 15);
+            debug_label.TabIndex = 7;
+            debug_label.Text = "debug_label";
+            debug_label.Click += label1_Click;
+            // 
+            // FFXIV_FBX_to_Fusion
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(496, 468);
+            Controls.Add(debug_label);
+            Controls.Add(fbx_path_label);
+            Controls.Add(blender_path_label);
+            Controls.Add(select_fbx_button);
+            Controls.Add(panel1);
+            Controls.Add(convert_button);
+            Controls.Add(blender_location_button);
+            Name = "FFXIV_FBX_to_Fusion";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button blender_location_button;
+        private Button convert_button;
+
+        private Panel panel1;
+        private RadioButton specular_button;
+        private RadioButton emissive_button;
+        private RadioButton opacity_button;
+        private RadioButton diffuse_button;
+        private Button select_fbx_button;
+        private Label blender_path_label;
+        private Label fbx_path_label;
+        private Label debug_label;
+    }
+}
---
+++ FFXIV FBX to Fusion/Form1.cs
@@ -0,0 +1,224 @@
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Collections;
+using System.Security;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Security.Cryptography;
+using System.Management.Automation.Runspaces;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FFXIV_FBX_to_Fusion
+{
+    public partial class FFXIV_FBX_to_Fusion : Form
+    {
+        private String? blender_path = null;
+        private String? fbx_path = null;
+
+
+        public FFXIV_FBX_to_Fusion()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Attempt to determine find default location for Blender binary
+            String[] drives = { "C:\\", "D:\\", "E:\\" };
+            foreach (String drive in drives)
+            {
+                String temp_path = drive + "Program Files\\Blender Foundation\\";
+                if (Directory.Exists(temp_path))
+                {
+                    String latest_path = Directory.GetDirectories(drive + "Program Files\\Blender Foundation\\")[Directory.GetDirectories(drive + "Program Files\\Blender Foundation\\").Length - 1];
+                    float version_number = float.Parse(FileVersionInfo.GetVersionInfo(latest_path + "\\blender.exe").FileVersion);
+                    if (version_number >= 4)
+                    {
+                        blender_path = latest_path + "\\blender.exe";
+                        blender_path_label.Text = blender_path;
+                    }
+                }
+                temp_path = drive + "Program Files (x86)\\Blender Foundation\\";
+                if (Directory.Exists(temp_path))
+                {
+                    String latest_path = Directory.GetDirectories(drive + "Program Files\\Blender Foundation\\")[Directory.GetDirectories(drive + "Program Files\\Blender Foundation\\").Length - 1];
+                    float version_number = float.Parse(FileVersionInfo.GetVersionInfo(latest_path + "\\blender.exe").FileVersion);
+                    if (version_number >= 4)
+                    {
+                        blender_path = latest_path + "\\blender.exe";
+                        blender_path_label.Text = blender_path;
+                    }
+                }
+            }
+            if (blender_path == null)
+            {
+                blender_path_label.Text = "Please provide Blender executive path; minimum version 4.0";
+            }
+
+            specular_button.Checked = true;
+        }
+        private void Convert()
+        {
+            if ((blender_path != null) && (fbx_path != null))
+            {
+                string working_path = System.IO.Path.GetDirectoryName(fbx_path);
+                string prefix = fbx_path.Split(working_path + "\\")[1].Split(".fbx")[0];
+
+                if (File.Exists(working_path + "\\mt_" + prefix + "_a_" + panel1.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked).Name.Substring(0, 1) + ".png"))
+                {
+                    using (PowerShell powerShell = PowerShell.Create())
+                    {
+                        string pythonFilePath = Path.Combine(Path.GetTempPath(), "ffxiv_fbx_to_fusion.py");
+
+                        string[] lines =
+                        {
+                            "import bpy",
+                            "import os",
+                            "import sys",
+                            "",
+                            "objs = bpy.data.objects",
+                            "objs.remove(objs['Cube'], do_unlink=True)"/*,
+                            "",
+                            "os.chdir(" + (working_path) + ")",
+                            "for file in os.listdir(os.getcwd()):",
+                            "	if file.endswith('fbx'):",
+                            "		bpy.ops.import_scene.fbx(filepath=os.path.join(os.getcwd(),file))",
+                            "		# File will be named after the last .fbx file found in the directory.",
+                            "		filename = file.split('.fbx')[0]",
+                            "",
+                            "try:",
+                            "	print(filename)",
+                            "except:",
+                            "	print('No fbx file found.')",
+                            "	exit()",
+                            "",
+                            "# Export to .obj format",
+                            "bpy.ops.wm.obj_export(filepath=os.path.join(os.getcwd(), filename + '.obj'))",
+                            "",
+                            "# Text manipulation to remove the bugged portion of the ",
+                            "with open(filename + '.mtl', 'r') as file: ",
+                            "  ",
+                            "    # Reading the content of the file ",
+                            "    # using the read() function and storing ",
+                            "    # them in a new variable ",
+                            "    data = file.read() ",
+                            "  ",
+                            "    # Searching and replacing the text ",
+                            "    # using the replace() function ",
+                            "    data = data.replace('C:/', '') ",
+                            "    data = data.replace(filename + '_a_d.png', filename + '_a_' + " + panel1.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked).Name.Substring(0, 1) + "'.png')",
+                            "  ",
+                            "# Opening our text file in write only ",
+                            "# mode to write the replaced content ",
+                            "with open(filename + '.mtl', 'w') as file: ",
+                            "  ",
+                            "    # Writing the replaced data in our ",
+                            "    # text file ",
+                            "    file.write(data)",
+                            "",
+                            "# Force use of specular for diffuse",
+                            "",
+                            "# Printing Text replaced ",
+                            "print('.mtl file repaired') "*/
+                        };
+
+                        using (StreamWriter outputFile = new StreamWriter(pythonFilePath))
+                        {
+                            foreach (string line in lines)
+                                outputFile.WriteLine(line);
+                        }
+
+                        if (File.Exists(pythonFilePath))
+                        {
+                            debug_label.Text = File.ReadAllText(pythonFilePath);
+                        }
+
+                        powerShell.AddCommand(blender_path)
+                            .AddParameter(" -P", pythonFilePath);
+
+                        //.AddParameter("P","E:\\project\\convert.py");
+
+                        Collection<PSObject> PSOutput = powerShell.Invoke();
+                    }
+
+                    //blender_path_label.Text = panel1.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked).Name.Substring(0, 1);
+                }
+                else
+                {
+                    // pop an alert prompting to locate their textures
+                }
+            }
+            else
+            {
+                // pop an alert prompting to populate an FBX
+            }
+        }
+
+        private void fbx_location_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new()
+            {
+                FileName = "",
+                Filter = "FBX Model|*.fbx",
+                Title = "Select FBX Model"
+            };
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                fbx_path = openFileDialog1.FileName;
+                fbx_path_label.Text = fbx_path; // <-- Shows file size in debugging mode.
+            }
+        }
+
+        private void convert_button_Click(object sender, EventArgs e)
+        {
+            Convert();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void blender_location_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new()
+            {
+                FileName = "blender.exe",
+                Filter = "Blender executable|blender.exe",
+                Title = "Select Blender executable"
+            };
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                blender_path = openFileDialog1.FileName;
+                blender_path_label.Text = blender_path; // <-- Shows file size in debugging mode.
+            }
+        }
+
+        private void blender_path_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
